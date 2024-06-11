9deafb07fa87da298ab221ec66b2885de9ff989c
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-06-12 07:40:27 +0900

    Create example

---
+++ examples/logging-tutorial.py
@@ -0,0 +1,10 @@
+import logging_tutorial.utils  # noqa: F401
+
+if __name__ == "__main__":
+    pass
+    # root_logger.setLevel(logging.DEBUG)
+    # root_logger.debug("debug message")
+    # root_logger.info("info message")
+    # root_logger.warning("warn message")
+    # root_logger.error("error message")
+    # root_logger.critical("critical message")
