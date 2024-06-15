3c0446a882ad05f13276bae8d1c778cf8583d855
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-06-15 14:32:50 +0900

    Create some logging module

--- src/logging_tutorial/logging/__init__.py
+++ src/logging_tutorial/logging/__init__.py
@@ -1,9 +1,23 @@
 from logging import CRITICAL, DEBUG, ERROR, INFO, NOTSET, WARNING
 
-from ._logging import get_logger
+from ._logging import (
+    catch_default_handler,
+    default_handler,
+    disable_default_handler,
+    enable_default_handler,
+    get_child_logger,
+    get_handler,
+    get_root_logger,
+)
 
 __all__ = (
-    "get_logger",
+    "catch_default_handler",
+    "default_handler",
+    "disable_default_handler",
+    "enable_default_handler",
+    "get_child_logger",
+    "get_handler",
+    "get_root_logger",
     "CRITICAL",
     "DEBUG",
     "ERROR",
